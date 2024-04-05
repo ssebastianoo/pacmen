1c0d193fae769e4f1f80ebd62881823773bb37c0
ssebastianoo <sebastianogirotto05@gmail.com>
2024-04-05 11:17:52 +0200

    custom host and port

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ import next from 'next';
 import { Server } from 'socket.io';
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
-const port = 3000;
+const hostname = process.env.HOST || 'localhost';
+const port = process.env.PORT || 3000;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
